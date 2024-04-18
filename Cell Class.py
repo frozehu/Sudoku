ea3778bb994b7a752e6590a5a22791526b3d054a
taylorschmitt <tayloreschmitt@gmail.com>
2024-04-18 18:31:24 -0400

    Created the class Cell.

---
+++ Cell Class.py
@@ -0,0 +1,34 @@
+import pygame
+class Cell:
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+        self.cell_size = 30
+        self.selected = False
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.value = value
+        #pass???
+
+    def draw(self):
+        x = self.row * self.cell_size
+        y = self.col * self.cell_size
+        rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
+        pygame.draw.rect(self.screen, (255, 255, 255), rect)
+        # if user selects a cell, outline in red
+        if self.selected:
+            pygame.draw.rect(self.screen, (255, 0, 0), rect, 3)
+        else:
+            return None
+        #draw cell and value inside it
+        if self.value != 0:
+            text = self.font.render(str(self.value), True, (0, 0, 0))
+            text_rect = text.get_rect(center = (x + self.cell_size/2, y + self.cell_size))
+            self.screen.blit(text, text_rect)
+        else:
+            return None
